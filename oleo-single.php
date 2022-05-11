0ab2653b13fa58f797cc323535e79b670aaed6a1
Gonçalo Antunes <goncalo.galhardo.antunes@gmail.com>
2022-05-11 13:44:28 +0100

    progresso recuperado

---
+++ oleo-single.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Oleo Single - Company Bootstrap Template</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+
+    <!-- CSS BASE-->
+    <?php
+    include("estruturaPrincipal/head-css.php");
+    ?>
+
+    <!-- JS BASE-->
+    <?php
+    include("estruturaPrincipal/head-js.php");
+    ?>
+
+    <!-- CSS PESSOAL -->
+    <link rel="stylesheet" href="estilo-post-criar.css">
+
+    <!-- EditorJS -->
+    <script src="https://cdn.jsdelivr.net/npm/@editorjs/header@latest"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@editorjs/paragraph@latest"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@editorjs/embed@latest"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@editorjs/delimiter@latest"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@editorjs/quote@latest"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@editorjs/image@2.3.0"></script>
+
+    <script src="editorJS/editor.js"></script>
+
+    <!-- Tags -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://unpkg.com/@yaireo/tagify/dist/tagify.css" rel="stylesheet" type="text/css" />
+
+    <link rel="stylesheet" href="cssPessoal/oleo-single.css">
+
+    <!-- =======================================================
+  * Template Name: Company - v4.6.1
+  * Template URL: https://bootstrapmade.com/company-free-html-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <!-- <pre> -->
+    <?php
+    // echo "espaco <br>";
+    // var_dump($_POST);
+    ?>
+    <!-- </pre> -->
+
+
+    <!-- ======= Header ======= -->
+    <?php
+    include("estruturaPrincipal/header.php");
+    ?>
+    <!-- End Header -->
+
+
+    <main id="main">
+
+        <!-- ======= Breadcrumbs ======= -->
+        <section id="breadcrumbs" class="breadcrumbs">
+            <div class="container">
+
+                <div class="d-flex justify-content-between align-items-center">
+                    <h2>Oleo Single</h2>
+                    <ol>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="blog.html">Placeholder</a></li>
+                        <li>Oleo Single</li>
+                    </ol>
+                </div>
+
+            </div>
+        </section><!-- End Breadcrumbs -->
+
+        <!-- ======= Blog Single Section ======= -->
+        <section id="blog" class="blog">
+            <div class="container" data-aos="fade-up">
+                <!-- Header -->
+                <div class="row">
+                    <div class="col-md-4 entries">
+                        <article class="entry entry-single">
+                            <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+                                <a href="/" class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+                                    <!-- Logo -->
+                                    <svg class="bi me-2" width="30" height="24">
+                                        <use xlink:href="#bootstrap"></use>
+                                    </svg>
+                                    <!-- Titulo da lista -->
+                                    <span class="fs-5 fw-semibold">List group</span>
+                                </a>
+                                <div class="list-group list-group-flush border-bottom scrollarea">
+                                    <a href="#" class="list-group-item list-group-item-action active py-3 lh-tight" aria-current="true">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small>Wed</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small class="text-muted">Tues</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small class="text-muted">Mon</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+
+                                    <a href="#" class="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small class="text-muted">Wed</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small class="text-muted">Tues</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small class="text-muted">Mon</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small class="text-muted">Wed</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small class="text-muted">Tues</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small class="text-muted">Mon</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small class="text-muted">Wed</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small class="text-muted">Tues</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action py-3 lh-tight">
+                                        <div class="d-flex w-100 align-items-center justify-content-between">
+                                            <strong class="mb-1">List group item heading</strong>
+                                            <small class="text-muted">Mon</small>
+                                        </div>
+                                        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
+                                    </a>
+                                </div>
+                            </div>
+                        </article>
+                        
+                    </div>
+                    <div class="col-md-8  entries">
+                        <article class="entry entry-single">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <h2>Nome Oleo</h1>
+                                    <h4 class="mt-5">Nome Oleo Portugues</h3>
+                                    
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="row">
+                                        <div class="col px-0" class="">
+                                            <img src="imgs/Aromatico.png" alt="Aromatico" class="img_fluid img-usos">
+                                        </div>
+                                        <div class="col px-0" class="">
+                                            <img src="imgs/Ingerir.png" alt="Aromatico" class="img-thumbnail img-usos">
+                                        </div>
+                                        <div class="col px-0" class="">
+                                            <img src="imgs/Topico.png" alt="Aromatico" class="img-thumbnail img-usos">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                        </article>
+                    </div>
+                </div>
+                <!-- Body -->
+                <div class="row">
+                    <div class="col">
+
+                    </div>
+                    <div class="col">
+                        
+                    </div>
+                    <div class="row">
+
+                    </div>
+                </div>
+        </section><!-- End Blog Single Section -->
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6 footer-contact">
+                        <h3>Company</h3>
+                        <p>
+                            A108 Adam Street <br>
+                            New York, NY 535022<br>
+                            United States <br><br>
+                            <strong>Phone:</strong> +1 5589 55488 55<br>
+                            <strong>Email:</strong> info@example.com<br>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-2 col-md-6 footer-links">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Our Services</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 footer-newsletter">
+                        <h4>Join Our Newsletter</h4>
+                        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                        <form action="" method="post">
+                            <input type="email" name="email"><input type="submit" value="Subscribe">
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container d-md-flex py-4">
+
+            <div class="me-md-auto text-center text-md-start">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>Company</span></strong>. All Rights Reserved
+                </div>
+                <div class="credits">
+                    <!-- All the links in the footer should remain intact. -->
+                    <!-- You can delete the links only if you purchased the pro version. -->
+                    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/company-free-html-bootstrap-template/ -->
+                    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                </div>
+            </div>
+            <div class="social-links text-center text-md-right pt-3 pt-md-0">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+        </div>
+    </footer><!-- End Footer -->
+</body>
+
+</html>
